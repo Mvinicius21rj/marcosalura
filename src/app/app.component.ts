5ecc4d6df2aca899e0a5205f31b648ec14b5e778
unknown <mviniciusrj21@gmail.com>
2020-09-10 17:43:17 -0300

    Primeiro commite

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic2';
+  url='https://img.r7.com/images/2014/08/13/6rkmyqljn5_2ue0e0m92w_file'
+
+}
